173bbf02af40f4dd1bf2c56c34648d6482385ac8
saint8083 <kvictoria040@gmail.com>
2023-09-01 12:05:49 -0700

    OBIG BST

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Performs a left-rotation on a binary tree
- * @tree: Pointer to the root node of the tree to check
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree: a pointer to the root node of the tree to check
+ *
  * Return: Pointer to the new root node of the tree once rotated
  *         NULL upon failure
  */
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - Performs a right-rotation on a binary tree
- * @tree: Pointer to the root node of the tree to check
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree
+ * @tree: a pointer to the root node of the tree to check
+ *
  * Return: Pointer to the new root node of the tree once rotated
  *         NULL upon failure
  */
