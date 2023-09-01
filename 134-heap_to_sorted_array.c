7c18569576d1b1d1e8b80ead83332543402de7c3
saint8083 <kvictoria040@gmail.com>
2023-09-01 02:17:07 -0700

    Max Binary Heap

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap
+ * to a sorted array of integers
+ * @heap: Pointer to the root node of the heap to convert
+ * @size: An address to store the size of the array
+ * Return: The generated array
+ *         NULL on failure
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+
+}
